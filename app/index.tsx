e2ca5b08dbc237117db60d61828387758898cad5
Nick Welch <nwelchr@gmail.com>
2024-10-23 13:49:58 -0700

    splash page

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,40 @@
-import { View, Text, Button } from "react-native";
-import { useRouter, Link } from "expo-router";
+import { ScrollView, View, Image, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 
-export default function Index() {
-  const router = useRouter();
+export default function App() {
   return (
-    <View className="flex flex-1 justify-center items-center">
-      <Text className="text-3xl font-pblack">Aora</Text>
-      <Link href="/home">Go home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            style={{ width: 130, height: 84 }}
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            style={{ maxWidth: 380, height: 300 }}
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              style={{ width: 136, height: 15 }}
+              className="absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limtless
+            exploration with Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
